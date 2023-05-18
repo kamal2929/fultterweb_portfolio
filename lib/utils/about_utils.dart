afdfc6f13d4a45be4401d8d31b3c335712a6f1df
kamal0029 <kamala.20it@kongu.edu>
2023-05-18 21:20:11 +0530

    website

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Kamal A, a Full stack developer, Technical blog writer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I\'m a Fresh Graduate with Information Technology as Majors from Kongu Engineering College,Perundurai. I have been developing website for over 2 years now. I have worked in teams for various teams and helped them in launching their prototypes and got valuable learning experience. Currently going to join Sirius Computer Solutions as Assosciate Consultant trainee';
+}
---
+++ lib/utils/contact_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactUtils {
+  static const List<IconData> contactIcon = [
+    Icons.home,
+    Icons.phone,
+    Icons.mail,
+  ];
+
+  static const List<String> titles = [
+    "Location",
+    "Phone",
+    "Email",
+  ];
+
+  static const List<String> details = [
+    "Hosur, TamilNadu",
+    "(+91)6369890623",
+    "kamal.anbazhagan29@gmail.com",
+  ];
+}
